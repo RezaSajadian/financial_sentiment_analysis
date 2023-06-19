78e09d9143d86a61198ca79ca9632bb530bd7e58
Reza Sajadian <Reza.sadjadian@gmail.com>
2023-06-19 16:37:29 +0100

    Update main_logic.py

--- main_logic.py
+++ main_logic.py
@@ -1,52 +1,73 @@
+# Import necessary libraries
 import logging
 from data_processing import prepare_data
-from model import get_model, get_trainer
+from model import get_model, get_optimizer
+from torch.utils.data import DataLoader
+from transformers import Trainer, TrainingArguments
+from config import get_config
 
-# Configure logging
-logging.basicConfig(filename='training.log', 
-                    level=logging.INFO, 
-                    format='%(asctime)s %(levelname)s:%(message)s', 
-                    datefmt='%m/%d/%Y %I:%M:%S %p')
+# Load the configuration parameters from the config.ini file
+config = get_config()
 
 def main():
-    """Main function that runs the script."""
-    # Log the start of the execution
-    logging.info("Started execution")
+    # This is the main function that is called to run the financial sentiment analysis project.
 
-    # Load the data from a text file
+    # Load the financial phrases dataset from the dataset.txt file
     with open('dataset.txt', 'r') as file:
         data = [line.strip().split(",") for line in file.readlines()]
-        financial_texts = [(item[0], { '-1': 0, '0': 1, '1': 2 }[item[1]]) for item in data]
+        phrases = [(item[0], int(item[1])) for item in data]
 
-    
-    # Use part of the data for testing (replace with actual test data in practice)
-    test_data = financial_texts[:50]
-
-    # Prepare the data
+    # Log the start of data preparation
     logging.info("Preparing data...")
-    train_dataset, test_dataset = prepare_data(financial_texts, test_data)
 
-    # Get the model
+    # Prepare the data for training and testing
+    train_dataset, test_dataset = prepare_data(phrases)
+
+    # Log the loading of the BERT model
     logging.info("Loading model...")
+
+    # Load the BERT model
     model = get_model()
 
-    # Get the trainer
-    logging.info("Initializing trainer...")
-    trainer = get_trainer(model, train_dataset)
+    # Log the setup of the training parameters
+    logging.info("Setting up training...")
+
+    # Compute the number of training steps and get the optimizer and scheduler
+    num_training_steps = len(train_dataset) * int(config.get('TRAINING', 'EPOCHS'))
+    optimizer, scheduler = get_optimizer(model, num_training_steps)
+
+    # Define the training arguments for the BERT model
+    training_args = TrainingArguments(
+        output_dir='./results',  # output directory
+        num_train_epochs=int(config.get('TRAINING', 'EPOCHS')),  # total number of training epochs
+        per_device_train_batch_size=int(config.get('TRAINING', 'BATCH_SIZE')),  # batch size per device during training
+        per_device_eval_batch_size=int(config.get('TRAINING', 'BATCH_SIZE')),  # batch size for evaluation
+        warmup_steps=500,  # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,  # strength of weight decay
+        logging_dir='./logs',  # directory for storing logs
+        logging_steps=10,
+    )
+
+    # Initialize the trainer with the BERT model and training arguments
+    trainer = Trainer(
+        model=model,  # the instantiated Transformers model to be trained
+        args=training_args,  # training arguments, defined above
+        train_dataset=train_dataset,  # training dataset
+        eval_dataset=test_dataset,  # evaluation dataset
+        optimizers=(optimizer, scheduler),  # optimizer and scheduler
+    )
+
+    # Log the start of training
+    logging.info("Starting training...")
 
     # Train the model
-    logging.info("Starting training...")
     trainer.train()
 
-    # Evaluate the model
+    # Log the start of model evaluation
     logging.info("Evaluating model...")
-    eval_results = trainer.evaluate(test_dataset)
 
-    # Log the evaluation results
-    logging.info(f"Evaluation results: {eval_results}")
+    # Evaluate the model
+    results = trainer.evaluate()
 
-    # Log the end of the execution
-    logging.info("Finished execution")
-
-if __name__ == "__main__":
-    main()
+    # Log the results of model evaluation
+    logging.info(f"Evaluation results: {results}")
